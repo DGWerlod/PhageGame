7a8fa12558010780a063f15b18aeb386b11e0f5e
Daniel <dgwerlod@users.noreply.github.com>
2020-05-25 11:36:07 -0700

    Added a way to type him good
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

---
+++ controls/keyboard.py
@@ -0,0 +1,20 @@
+import pygame
+import constants
+
+pygame.init()
+
+presets = {'keyW': pygame.K_w, 'keyA': pygame.K_a, 'keyS': pygame.K_s, 'keyD': pygame.K_d, 'keySpace': pygame.K_SPACE,
+           'keyEnter': pygame.K_RETURN, 'keyEscape': pygame.K_ESCAPE}
+controls = {'keyW': False, 'keyA': False, 'keyS': False, 'keyD': False, 'keySpace': False, 'keyEnter': False,
+            'keyEscape': False}
+
+
+def listen(event):
+    if event.type == pygame.KEYDOWN:
+        for p in presets:
+            if event.key == presets[p]:
+                controls[p] = True
+    elif event.type == pygame.KEYUP:
+        for p in presets:
+            if event.key == presets[p]:
+                controls[p] = False
