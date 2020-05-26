bb697e2ee9203afaf9936733ae07b5617128f582
Desmond <desitootoo+github@gmail.com>
2020-05-26 17:38:21 -0500

    who am i

--- img/images.py
+++ img/images.py
@@ -1,3 +1,14 @@
-IMAGES = {
+import pygame.image
 
+IMAGES = {
+    "buffteriophage":  {"walk":    [pygame.image.load("img/buffteriophage/frame1.png"),
+                                    pygame.image.load("img/buffteriophage/frame2.png"),
+                                    pygame.image.load("img/buffteriophage/frame3.png"),
+                                    pygame.image.load("img/buffteriophage/frame4.png"),
+                                    pygame.image.load("img/buffteriophage/frame5.png"),
+                                    pygame.image.load("img/buffteriophage/frame6.png"),
+                                    pygame.image.load("img/buffteriophage/frame7.png"),
+                                    pygame.image.load("img/buffteriophage/frame8.png")],
+                        "static":  [pygame.image.load("img/buffteriophage/frame3.png")]
+                      }
 }
