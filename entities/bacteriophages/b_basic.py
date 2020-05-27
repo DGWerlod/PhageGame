b158f10ebf00adf28da0a6521d02c010ec340e8c
DGWerlod <ddeanda333@gmail.com>
2020-05-27 16:17:08 -0700

    Fixed that time that I was dumb
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- entities/bacteriophages/b_basic.py
+++ entities/bacteriophages/b_basic.py
@@ -4,4 +4,5 @@ from entities.bacteriophages.bacteriophage import Bacteriophage
 
 class B_Basic(Bacteriophage):
     def __init__(self):
-        super().__init__(0, constants.GAME_HEIGHT / 2, 100, 125, 5, 1, 1, "buffteriophage", "walk", 5)
+        super().__init__(constants.GAME_WIDTH - 100, constants.GAME_HEIGHT / 2,
+                         100, 125, 5, 1, 1, "buffteriophage", "walk", 5)
