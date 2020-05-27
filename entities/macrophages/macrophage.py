125adccba0910d320a560f0002af95614f5cdd53
Desmond <desitootoo+github@gmail.com>
2020-05-27 17:32:11 -0500

    added macrophage images

--- entities/macrophages/macrophage.py
+++ entities/macrophages/macrophage.py
@@ -1,6 +1,10 @@
 from entities.microbe import Microbe
-
+from img.images import IMAGES
 
 class Macrophage(Microbe):
     def __init__(self, x, y, w, h, spd, hp, dmg, name=None):
         super().__init__(x, y, w, h, spd, hp, dmg, name)
+
+    def go(self, context, in_front):
+            self._cycle = (self._cycle + 1) % (8*self._spd)
+            context.blit(IMAGES[self._name]["walk"][self._cycle // self._spd], (self._x, self._y)) 
--- img/images.py
+++ img/images.py
@@ -10,5 +10,15 @@ IMAGES = {
                                     pygame.image.load("img/buffteriophage/frame7.png"),
                                     pygame.image.load("img/buffteriophage/frame8.png")],
                         "static":  [pygame.image.load("img/buffteriophage/frame3.png")]
-                      }
+                       },
+    "macrophoot":      {"walk":    [pygame.image.load("img/macrophoot/frame1.png"),
+                                    pygame.image.load("img/macrophoot/frame2.png"),
+                                    pygame.image.load("img/macrophoot/frame3.png"),
+                                    pygame.image.load("img/macrophoot/frame4.png"),
+                                    pygame.image.load("img/macrophoot/frame5.png"),
+                                    pygame.image.load("img/macrophoot/frame6.png"),
+                                    pygame.image.load("img/macrophoot/frame7.png"),
+                                    pygame.image.load("img/macrophoot/frame8.png")],
+                        "static":  [pygame.image.load("img/macrophoot/frame1.png")]
+                       }
 }
