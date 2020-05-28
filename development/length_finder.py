60072025249a4314c0c24a9f3cc92854365a37a1
DGWerlod <ddeanda333@gmail.com>
2020-05-27 18:02:42 -0700

    Adjusted length finder
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- development/length_finder.py
+++ development/length_finder.py
@@ -1,15 +1,17 @@
 files = ["../constants.py", "../level.py", "../main.py",
          "../controls/keyboard.py", "../controls/mouse.py",
          "../fonts/text.py", "../img/images.py",
-         "../logic/collisions.py", "../logic/graphics.py", "../logic/rect.py", "../logic/circle.py"
+         "../logic/collisions.py", "../logic/graphics.py", "../logic/rect.py", "../logic/circle.py",
          "../entities/entity.py", "../entities/microbe.py", "../entities/hud.py",
+         "../entities/buildings/base.py", "../entities/buildings/wall.py",
          "../entities/macrophages/macrophage.py", "../entities/bacteriophages/bacteriophage.py",
+         "../entities/macrophages/m_basic.py", "../entities/bacteriophages/b_basic.py",
          "../entities/buttons/button.py", "../entities/buttons/system.py", "../entities/buttons/upgrade.py",
          "../entities/buttons/summoner.py"]
 
 
-def file_len(fName):
-    with open(fName) as file:
+def file_len(f_name):
+    with open(f_name) as file:
         i = 0
         for i, l in enumerate(file):
             pass
