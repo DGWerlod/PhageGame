932f997616d065f92e27abe5ac95974443f18402
DGWerlod <ddeanda333@gmail.com>
2020-05-27 17:31:51 -0700

    Fixed image proportions
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- img/images.py
+++ img/images.py
@@ -1,28 +1,50 @@
 import pygame.image
 
 IMAGES = {
-    "buffteriophage": {"walk":  [pygame.image.load("img/buffteriophage/frame1.png"),
-                                 pygame.image.load("img/buffteriophage/frame2.png"),
-                                 pygame.image.load("img/buffteriophage/frame3.png"),
-                                 pygame.image.load("img/buffteriophage/frame4.png"),
-                                 pygame.image.load("img/buffteriophage/frame5.png"),
-                                 pygame.image.load("img/buffteriophage/frame6.png"),
-                                 pygame.image.load("img/buffteriophage/frame7.png"),
-                                 pygame.image.load("img/buffteriophage/frame8.png")],
-                       "static": [pygame.image.load("img/buffteriophage/frame3.png")]
-                       },
-    "macrophoot": {"walk": [pygame.image.load("img/macrophoot/frame1.png"),
-                            pygame.image.load("img/macrophoot/frame2.png"),
-                            pygame.image.load("img/macrophoot/frame3.png"),
-                            pygame.image.load("img/macrophoot/frame4.png"),
-                            pygame.image.load("img/macrophoot/frame5.png"),
-                            pygame.image.load("img/macrophoot/frame6.png"),
-                            pygame.image.load("img/macrophoot/frame7.png"),
-                            pygame.image.load("img/macrophoot/frame8.png")],
-                   "static": [pygame.image.load("img/macrophoot/frame1.png")]
-                   },
-    "macrophage_summoner":    {"active": [pygame.image.load("img/buttons/footbutton.png")],
-                               "inactive": [pygame.image.load("img/buttons/footbutton.png")]},
-    "bacteriophage_summoner": {"active": [pygame.image.load("img/buttons/buffbutton.png")],
-                               "inactive": [pygame.image.load("img/buttons/buffbutton.png")]},
+    "buffteriophage": {
+        "walk": [
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame1.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame2.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame3.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame4.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame5.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame6.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame7.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame8.png"), (125, 125))
+        ],
+        "static": [
+            pygame.transform.scale(pygame.image.load("img/buffteriophage/frame3.png"), (125, 125))
+        ]
+    },
+    "macrophoot": {
+        "walk": [
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame1.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame2.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame3.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame4.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame5.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame6.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame7.png"), (125, 125)),
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame8.png"), (125, 125))
+        ],
+        "static": [
+            pygame.transform.scale(pygame.image.load("img/macrophoot/frame1.png"), (125, 125))
+        ]
+    },
+    "macrophage_summoner": {
+        "active": [
+            pygame.image.load("img/buttons/footbutton.png")
+        ],
+        "inactive": [
+            pygame.image.load("img/buttons/footbutton.png")
+        ]
+    },
+    "bacteriophage_summoner": {
+        "active": [
+            pygame.image.load("img/buttons/buffbutton.png")
+        ],
+        "inactive": [
+            pygame.image.load("img/buttons/buffbutton.png")
+        ]
+    },
 }
