d716419c307353f1388b7d857afa4f961e4b6d21
Desmond <desitootoo+github@gmail.com>
2020-06-07 14:03:58 -0500

    Summoners now show cooldown

--- entities/buttons/summoner.py
+++ entities/buttons/summoner.py
@@ -1,15 +1,17 @@
 import pygame
 from pygame.rect import Rect
-
+from pygame import Surface
 import constants
 from entities.buttons.button import Button
-
+from entities.microbes.microbe_builder import build_microbe
 
 class Summoner(Button):
+
     def __init__(self, x, y, w, h, unit_id):
-        self._cooldown_timer = 30
+        self._cooldown_timer = max(30, build_microbe(unit_id).get_hp() // 3)
         self._cooldown_left = 0
         self._unit_ID = unit_id
+        self._current_animation = "active"
         name = unit_id + "_summoner"
         super().__init__(x, y, w, h, name)
 
@@ -25,15 +27,14 @@ class Summoner(Button):
         else:
             raise AssertionError("Invalid summoning!")
 
-    def go(self, display):
-
-        if self.can_summon():
-            self._current_animation = "active"
-        else:
-            self._current_animation = "inactive"
-        
+    def go(self, display):        
         super().go(display)
 
+        shade = Surface((self._w, self._h * self._cooldown_left // self._cooldown_timer))
+        shade.set_alpha(63)
+        shade.fill(constants.BLACK)
+        display.blit(shade, (self._x, self._y + (self._h * (self._cooldown_timer - self._cooldown_left) // self._cooldown_timer)))
+        
         pygame.draw.rect(display, constants.BLACK, Rect(self._x, self._y, self._w, self._h), 2)
 
         self._cooldown_left = max(0, self._cooldown_left - 1)
--- entities/microbes/bacteriophages/b_pult.py
+++ entities/microbes/bacteriophages/b_pult.py
@@ -10,7 +10,7 @@ class B_Pult(Bacteriophage):
 
     def __init__(self):
         super().__init__(constants.GAME_WIDTH - 125, constants.CENTER_Y,
-                         125, 125, 7, 30, 0, constants.B_PULT, 1, 2)
+                         125, 125, 7, 20, 0, constants.B_PULT, 1, 2)
 
     def make_projectile(self):
         return Projectile(self._x, self._y, 50, 50, 40, B_Pult.DIE.roll(), self)
